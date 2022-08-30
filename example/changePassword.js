354bcaf56b6777b53751be87b5049763d80cfb89
Cyril Walle <cyril.walle@protonmail.com>
2022-08-30 20:05:52 +0200

    add example

---
+++ example/changePassword.js
@@ -0,0 +1,7 @@
+// deno run --allow-write="./example/hashedPassword.json" ./example/changePassword.js
+import {hashPasswordWithRandomSalt} from "../lib/passwuert.js";
+
+const password = prompt("password to save");
+const hashed = await hashPasswordWithRandomSalt(password);
+await Deno.writeTextFile("./example/hashedPassword.json", JSON.stringify(hashed));
+console.log("saved");
---
+++ example/viewSecret.js
@@ -0,0 +1,22 @@
+// run changePassword first
+// deno run --allow-read="./example/hashedPassword.json","./example/secret.txt" ./example/viewSecret.js
+import {comparePasswordToHashed} from "../lib/passwuert.js";
+
+const password = prompt("password");
+let savedData;
+try {
+    savedData = await Deno.readTextFile("./example/hashedPassword.json")
+} catch (probablyNotFoundError) {
+    console.log("Password not found. Use changePassword.js first.")
+}
+
+const hashed = JSON.parse(savedData)
+    
+const isCorrect = await comparePasswordToHashed(password,hashed);
+if (!isCorrect) {
+    throw "incorrect password";
+} else {
+    console.log("password corect");
+}
+console.log("secret.txt content:");
+console.log(await Deno.readTextFile("./example/secret.txt"));
